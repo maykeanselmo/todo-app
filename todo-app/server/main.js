d19133a57c682d303426af32a06a15d9f18852fb
Mayke <mayke000000000@gmail.com>
2025-02-25 14:02:50 -0300

    feat: adição do insert no server

--- todo-app/server/main.js
+++ todo-app/server/main.js
@@ -1,9 +1,18 @@
 import { Meteor } from 'meteor/meteor';
-
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '../imports/api/TasksCollection';
+import "../imports/api/TasksPublications"; 
+import "../imports/api/tasksMethods"; 
 
 
 const SEED_USERNAME = 'meteorite';
 const SEED_PASSWORD = 'password';
+
+const insertTask = (taskText, time) =>
+  TasksCollection.insertAsync({ 
+    text: taskText,
+    time: time
+  });
 
 Meteor.startup(async () => {
   const user = await Accounts.findUserByUsername(SEED_USERNAME);
@@ -12,5 +21,16 @@ Meteor.startup(async () => {
       username: SEED_USERNAME,
       password: SEED_PASSWORD,
     });
+  }
+  if ((await TasksCollection.find().countAsync()) === 0) {
+    const mockTasks = [
+      { text: "Comprar leite", time: new Date() },
+      { text: "Estudar React", time: new Date() },
+      { text: "Fazer exercícios", time: new Date() }
+    ];
+
+    for (const task of mockTasks) {
+      await insertTask(task.text, task.time);
+    }
   }
 });
