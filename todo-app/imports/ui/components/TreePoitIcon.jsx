8ff32fbc2e776625313456880d16ebb6b67fcf41
Mayke <mayke000000000@gmail.com>
2025-02-26 13:54:26 -0300

    refac: Deleção de comentários

--- todo-app/imports/ui/components/TreePoitIcon.jsx
+++ todo-app/imports/ui/components/TreePoitIcon.jsx
@@ -20,13 +20,6 @@ const FontAwesomeSvgIcon = React.forwardRef((props, ref) => {
       {typeof svgPathData === 'string' ? (
         <path d={svgPathData} />
       ) : (
-        /**
-         * A multi-path Font Awesome icon seems to imply a duotune icon. The 0th path seems to
-         * be the faded element (referred to as the "secondary" path in the Font Awesome docs)
-         * of a duotone icon. 40% is the default opacity.
-         *
-         * @see https://fontawesome.com/how-to-use/on-the-web/styling/duotone-icons#changing-opacity
-         */
         svgPathData.map((d, i) => (
           <path style={{ opacity: i === 0 ? 0.4 : 1 }} d={d} />
         ))
