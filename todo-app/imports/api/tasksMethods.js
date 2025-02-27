4a7a70562a8d7dce64bc339ad8771a1ce7f00795
Mayke <mayke000000000@gmail.com>
2025-02-27 15:57:07 -0300

    feat: método update

--- todo-app/imports/api/tasksMethods.js
+++ todo-app/imports/api/tasksMethods.js
@@ -7,5 +7,22 @@ Meteor.methods({
   },
   "tasks.delete"(_id){
     return TasksCollection.removeAsync(_id);
-  }
+  },
+  "tasks.update"(taskId, taskUpdated){
+    if (!this.userId) {
+      throw new Meteor.Error("Não autorizado", "Você precisa estar logado para atualizar uma tarefa.");
+    }
+    return TasksCollection.updateAsync(
+      { _id: taskId },
+      {
+        $set: {
+          name: taskUpdated.name,
+          description: taskUpdated.description,
+          date: taskUpdated.date,
+          updatedAt: new Date(),
+        },
+      }
+    );
+  },
+
 });
