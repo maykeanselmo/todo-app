7cb6c153e89f14a13a3e6c6fe705540dc2a4b725
Mayke <mayke000000000@gmail.com>
2025-03-05 14:38:55 -0300

    feat: status das tasks

--- todo-app/imports/ui/components/TaskStatus.jsx
+++ todo-app/imports/ui/components/TaskStatus.jsx
@@ -1,11 +1,37 @@
 import * as React from "react";
+import { Meteor } from "meteor/meteor";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
+import { useTracker } from "meteor/react-meteor-data";
+import { TasksCollection } from "../../api/TasksCollection";
 
-export const TaskStatus = () => {
+export const TaskStatus = ({task}) => {
+  
+const isInRegistered =  'CADASTRADO';
+const isInProgress =  'ANDAMENTO';
+const isCompleted =  'CONCLUÍDO';
+
+
+
+const taskReturned = useTracker(() => {
+    return TasksCollection.findOne({ _id: task });
+  });
+
+const setStatus = (status) => {
+    if(taskReturned.status === isInRegistered && status === isCompleted){
+      return;
+    }
+    else{
+      Meteor.callAsync("tasks.setStatus", task, status)
+    }
+  }
+
+
+
   return (
     <ButtonGroup variant="contained" aria-label="Basic button group">
       <Button
+        onClick={ () => setStatus(isInRegistered)}
         sx={{
           backgroundColor: "#bdbdbd",
           color: "#fff",
@@ -16,16 +42,19 @@ export const TaskStatus = () => {
       </Button>
 
       <Button
+      onClick={ () => setStatus(isInProgress)}
         sx={{
           backgroundColor: "#64b5f6", 
           color: "#fff",
           "&:hover": { backgroundColor: "#42a5f5" }
         }}
+        
       >
         Andamento
       </Button>
 
       <Button
+      onClick={ () => setStatus(isCompleted)}
         sx={{
           backgroundColor: "#66bb6a", 
           color: "#fff",
