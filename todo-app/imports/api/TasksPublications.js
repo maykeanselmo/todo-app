c3ae6dc4cd485d71b00168e9cea6ed7670d00d0d
Mayke <mayke000000000@gmail.com>
2025-03-05 16:21:02 -0300

    feat: filtro no publisher

--- todo-app/imports/api/TasksPublications.js
+++ todo-app/imports/api/TasksPublications.js
@@ -1,5 +1,21 @@
 import { TasksCollection } from "./TasksCollection";
+import { Meteor } from "meteor/meteor";
 
-Meteor.publish("tasks", () => {
-    return TasksCollection.find();
+Meteor.publish("tasks", async function () {
+  console.log("cheguei no publisher");
+
+  if (!this.userId) {
+    return [];
+  }
+
+  const currentUser = await Meteor.users.findOneAsync(this.userId);
+  const currentUsername = currentUser ? currentUser.username : '';
+
+  return TasksCollection.find({
+    $or: [
+      { typeTask: 'public' },
+    
+      { typeTask: 'personal', createBy: currentUsername }
+    ]
   });
+});
