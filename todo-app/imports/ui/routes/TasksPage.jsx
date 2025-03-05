2f9f3b3bbeac58b39fd141867ea228da7dac96a3
Mayke <mayke000000000@gmail.com>
2025-03-05 14:36:16 -0300

    refact: remoção de task options

--- todo-app/imports/ui/routes/TasksPage.jsx
+++ todo-app/imports/ui/routes/TasksPage.jsx
@@ -4,14 +4,12 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { useNavigate } from 'react-router-dom';
 import { TaskList } from '../components/TaskList';
 import { TaskForm } from "../components/TaskForm.jsx";
-import { TasksCollection } from "../../api/TasksCollection"; 
 import "./TasksPage.css"; 
 import { AddTaskButton } from "../components/AddTaskButton.jsx";
-import { TaskOptionsButton } from "../components/TaskOptionsButton.jsx";
 
 export const TasksPage = () => {
     const user = useTracker(() => Meteor.user());
-    const tasks = useTracker(() => TasksCollection.find({}).fetch());
+   
     const [isFormVisible, setFormVisible] = useState(false);
     const navigate = useNavigate();
 
@@ -27,9 +25,7 @@ export const TasksPage = () => {
         <>
             <div className="tasks-page">
                 <h1 className="tasks-title">📌 Minhas Tarefas</h1>
-
-                <TaskOptionsButton/>
-              
+                
                 <TaskList />
                 <AddTaskButton onClick={toggleFormVisibility} />
                 {isFormVisible && <TaskForm />}
