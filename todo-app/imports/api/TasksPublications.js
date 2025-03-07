e4592a00fedaa6f14620b9459f5a48ebed790950
Mayke <mayke000000000@gmail.com>
2025-03-07 13:55:14 -0300

    refac: taskPublications  filtra por id

--- todo-app/imports/api/TasksPublications.js
+++ todo-app/imports/api/TasksPublications.js
@@ -2,20 +2,16 @@ import { TasksCollection } from "./TasksCollection";
 import { Meteor } from "meteor/meteor";
 
 Meteor.publish("tasks", async function () {
-  console.log("cheguei no publisher");
 
   if (!this.userId) {
     return [];
   }
 
-  const currentUser = await Meteor.users.findOneAsync(this.userId);
-  const currentUsername = currentUser ? currentUser.username : '';
-
   return TasksCollection.find({
     $or: [
       { typeTask: 'public' },
     
-      { typeTask: 'personal', createBy: currentUsername }
+      { typeTask: 'personal', createByUser: this.userId }
     ]
   });
 });
