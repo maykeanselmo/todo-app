a42cfd19b8d2e2918b02d3368473ff88ae218076
Mayke <mayke000000000@gmail.com>
2025-03-11 15:58:51 -0300

    feat: adição do drawer

--- todo-app/imports/ui/routes/UserProfilePage.jsx
+++ todo-app/imports/ui/routes/UserProfilePage.jsx
@@ -2,16 +2,25 @@ import * as React from "react";
 import "./UserProfilePage.css"; 
 import { Profile } from "../components/Profile.jsx";
 import { useTracker } from 'meteor/react-meteor-data';
+import { TemporaryDrawer } from "../components/TemporaryDrawer.jsx";
+import { useNavigate } from 'react-router-dom';
 
 export const UserProfilePage = () => {
  
   const user = useTracker(() => Meteor.user());
+  const navigate = useNavigate();
 
+  const login = () => {
+    navigate('/');
+}
   return (
     <div className="profile-container">
        <div>
              {user ? (
                 <div>
+                    <div>
+                      <TemporaryDrawer/> 
+                    </div>
                   
                     <Profile/>
                 </div>
@@ -19,7 +28,7 @@ export const UserProfilePage = () => {
             : (
                 <div>
                     <h1>Faça login!</h1>
-                    
+                    <button onClick={login}>Fazer login</button>
                 </div>
             )
             }
